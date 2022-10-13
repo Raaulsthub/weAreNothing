e9e72b34832d66fba13dcfa24c8a052bc69cdeba
Raaulsthub <rsteinmetz@inf.ufsm.br>
2022-10-13 11:57:54 -0300

    Adding the enemy base to game

--- Assets/Scripts/Bosses/DemonGuardian/DemonGuardianCombat.cs
+++ Assets/Scripts/Bosses/DemonGuardian/DemonGuardianCombat.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class DemonGuardianCombat : MonoBehaviour
 {
-	
+		
     // Start is called before the first frame update
     void Start()
     {
-     	  
+     
+  	  
     }
 
     // Update is called once per frame
