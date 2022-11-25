ac139badb263b4f85dd4aff3a495bb1a7e2bfcc1
robissu <57465428+robissu@users.noreply.github.com>
2022-11-25 00:33:02 -0300

    Add files via upload

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,23 +5,22 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     Rigidbody2D myRigidBody;
-    
     CapsuleCollider2D myCollider;
     BoxCollider2D feetCollider;
     private bool grounded;
 
-    private bool isJumping;
-    private float jumpTime;
-    public float jumpQuantity;
+    private bool pulando;
+    private float tempoPulo;
+    public float puloQtd;
     private bool doubleJump;
     Animator myAnimator;
     private float moveInputX;
     [SerializeField] private float jumpSpeed;
     [SerializeField] private float moveSpeed;
     
-
     void Start()
     {
+        //Physics2D.IgnoreLayerCollision(6, 7);
         myRigidBody = GetComponent<Rigidbody2D>();
         myCollider = GetComponent<CapsuleCollider2D>(); 
         myAnimator = GetComponent<Animator>();
@@ -31,16 +30,16 @@ public class PlayerMovement : MonoBehaviour
 
     void FixedUpdate(){
         moveInputX = Input.GetAxisRaw("Horizontal");
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
+        if(Input.GetKey(KeyCode.LeftShift)){
             moveInputX *= 1.5f;
         }
-
+            
         Run();
-        Jump();
+        
     }
     void Update()
     {
+        Jump();
         FlipSprite();
     }
 
@@ -51,34 +50,34 @@ public class PlayerMovement : MonoBehaviour
     }
     void Jump()
     {
-        if(Input.GetKeyDown(KeyCode.Space) && (grounded || doubleJump)){
+        if(Input.GetKeyDown(KeyCode.Space) && grounded){
             myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, jumpSpeed);
             myAnimator.SetBool("isJumping", true);
             grounded = false;
 
-            jumpTime = jumpQuantity;
-            isJumping = true;
+            tempoPulo = puloQtd;
+            pulando = true;
             doubleJump = true;
         }
         else if(Input.GetKeyDown(KeyCode.Space) && doubleJump){
             myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, jumpSpeed);
             myAnimator.SetBool("isJumping", true);
 
-            jumpTime = jumpQuantity;
-            isJumping = true;
+            tempoPulo = puloQtd;
+            pulando = true;
             doubleJump = false;
         }
-        if(Input.GetKey(KeyCode.Space) && isJumping == true){
-            if(jumpTime > 0){
+        if(Input.GetKey(KeyCode.Space) && pulando == true){
+            if(tempoPulo > 0){
                 myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, jumpSpeed);
-                jumpTime -= Time.deltaTime;
+                tempoPulo -= Time.deltaTime;
             }
             else{
-                isJumping = false;
+                pulando = false;
             }
         }
         if(Input.GetKeyUp(KeyCode.Space)){
-            isJumping = false;
+            pulando = false;
         }
         
     }
@@ -90,6 +89,7 @@ public class PlayerMovement : MonoBehaviour
             grounded = true;
             myAnimator.SetBool("isJumping", false);
         }
+        
     }
 
     void FlipSprite()
